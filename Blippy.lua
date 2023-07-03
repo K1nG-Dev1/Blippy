070b7704d7c6db2b87f8ed8d0a2a0f7c8cc391e1
K1nG-Dev1 <88916437+K1nG-Dev1@users.noreply.github.com>
2023-07-03 18:41:49 -0400

    Update Blippy.lua

--- Blippy.lua
+++ Blippy.lua
@@ -1,9 +1,7 @@
 -- Blippy.lua
 
--- Create the addon namespace
 Blippy = {}
 
--- Event handler for addon initialization
 function Blippy.OnLoad()
     -- Register events for addon
     this:RegisterEvent("PLAYER_LOGIN")
@@ -11,9 +9,7 @@ function Blippy.OnLoad()
     this:RegisterEvent("PLAYER_LEAVE_COMBAT")
 end
 
--- Event handler for player login
 function Blippy.OnPlayerLogin()
-    -- Load saved variables
     Blippy.LoadSavedVariables()
 
     -- Addon initialization code
@@ -22,17 +18,13 @@ function Blippy.OnPlayerLogin()
     print("Loaded variable:", Blippy.savedVariable)
 end
 
--- Event handler for player entering the world
 function Blippy.OnPlayerEnteringWorld()
-    -- Check if the player is in an instance
     if IsInInstance() then
-        -- Get player and instance information
         local characterName = UnitName("player")
         local instanceName = GetInstanceInfo()
         local instanceID = GetInstanceID()
         local enterTime = date("%H:%M:%S") -- Local current time as EnterTime
 
-        -- Create a new entry in saved variables
         Blippy.savedVariables = Blippy.savedVariables or {}
         table.insert(Blippy.savedVariables, {
             characterName = characterName,
@@ -44,47 +36,38 @@ function Blippy.OnPlayerEnteringWorld()
 
         print("Data stored for instance:", instanceName)
 
-        -- Save the updated variables
         Blippy.SaveVariables()
     end
 end
 
--- Event handler for player leaving combat
 function Blippy.OnPlayerLeaveCombat()
-    -- Check if the player is no longer in an instance
     if not IsInInstance() then
         local lastEntry = Blippy.savedVariables[#Blippy.savedVariables]
         if lastEntry and not lastEntry.ExitTime then
-            -- Update the exit time with the current local time
             lastEntry.ExitTime = date("%H:%M:%S") -- Local current time as ExitTime
 
             print("Data updated for instance:", lastEntry.instanceName)
 
-            -- Save the updated variables
             Blippy.SaveVariables()
         end
     end
 end
 
--- Function to load saved variables
 function Blippy.LoadSavedVariables()
-    if BlippySavedVariables then
-        Blippy.savedVariables = BlippySavedVariables
-    end
+    -- Load the saved variables from the account-wide file
+    BlippySavedVariables = BlippySavedVariables or {}
 
-    if not Blippy.savedVariables then
-        Blippy.savedVariables = {}
-    end
+    -- Assign the saved variables to the addon's table
+    Blippy.savedVariables = BlippySavedVariables
 end
 
--- Function to save variables
 function Blippy.SaveVariables()
+    -- Assign the addon's table to the saved variables
     BlippySavedVariables = Blippy.savedVariables
 
     print("Variables saved")
 end
 
--- Register event handlers and set event frame
 Blippy.OnLoad()
 BlippyFrame:SetScript("OnEvent", function(self, event, ...)
     Blippy[event](self, ...)
